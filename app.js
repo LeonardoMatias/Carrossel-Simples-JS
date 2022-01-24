59e44d2fba376ec9d0a41f69fa0eeec9083b08a3
Leonardo Matias <84511975+LeonardoMatias@users.noreply.github.com>
2022-01-24 17:46:03 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const slides = document.querySelectorAll('[data-js="carousel__item"]') 
+const nextButton = document.querySelector('[data-js="carousel__button--next"]')
+const prevButton = document.querySelector('[data-js="carousel__button--prev"]')
+
+let currentSlideIndex = 0
+
+//Evento do botão Next do carrossel
+nextButton.addEventListener('click', () => {
+  if (currentSlideIndex === slides.length - 1){
+    currentSlideIndex = 0
+  } else {
+    currentSlideIndex++
+  }
+    slides.forEach(slide => {
+      slide.classList.remove('carousel__item--visible')
+    })
+    slides[currentSlideIndex].classList.add('carousel__item--visible')  
+})
+
+//Evento do botão Prev do carrosel
+prevButton.addEventListener('click', () => {
+  if (currentSlideIndex === 0){
+    currentSlideIndex = slides.length -1
+  } else {
+    --currentSlideIndex
+  } 
+  slides.forEach(slide => {
+    slide.classList.remove('carousel__item--visible')
+  })
+  slides[currentSlideIndex].classList.add('carousel__item--visible')
+})
